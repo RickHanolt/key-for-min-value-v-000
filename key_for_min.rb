8d3cc775da2830b1e087c4395c67a538703ce378
Richard Hanolt <rhanolt@gmail.com>
2020-05-08 22:19:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,18 +6,18 @@ def key_for_min_value(values) #{:blake => 500, :ashley => 2, :adam => 1}
     temp_value = nil
     temp_key = nil
   values.each do |key, value|
-    
+
     if temp_value == nil
       temp_value = value
       temp_key = key
     end
-    
+
     if value < temp_value
       temp_value = value
       temp_key = key
     end
-    
+
   end
-  
+
   return temp_key
 end
